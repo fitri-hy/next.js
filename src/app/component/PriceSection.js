6c97d98182ba1f3174ff7dc73c9fc539e05ebcf7
fitri-hy <155282015+fitri-hy@users.noreply.github.com>
2023-12-31 12:16:58 +0700

    PriceSection.js

--- src/app/component/PriceSection.js
+++ src/app/component/PriceSection.js
@@ -30,20 +30,20 @@ export default function PriceSection() {
 					  <h4 data-aos="fade-up" class="flex-none text-sm font-semibold leading-6 text-indigo-600">What’s included</h4>
 					  <div class="h-px flex-auto bg-gray-100"></div>
 					</div>
-					<ul role="list" class="mt-8 grid grid-cols-1 gap-4 text-sm leading-6 text-gray-600 sm:grid-cols-2 sm:gap-6">
-					  <li data-aos="fade-left" class="flex gap-x-3">
+					<ul role="list" class="mt-8 grid grid-cols-2 gap-4 text-sm leading-6 text-gray-600 sm:grid-cols-2 sm:gap-6">
+					  <li class="flex gap-x-3">
 						<svg class="h-6 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
 						  <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
 						</svg>
 						Lifetime Maintenance
 					  </li>
-					  <li data-aos="fade-left" class="flex gap-x-3">
+					  <li data-aos="fade-up" class="flex gap-x-3">
 						<svg class="h-6 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
 						  <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
 						</svg>
 						Free Consultation
 					  </li>
-					  <li data-aos="fade-left" class="flex gap-x-3">
+					  <li data-aos="fade-up" class="flex gap-x-3">
 						<svg class="h-6 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
 						  <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
 						</svg>
@@ -57,7 +57,7 @@ export default function PriceSection() {
 					  </li>
 					</ul>
 				  </div>
-				  <div class="drop-shadow-md -mt-2 p-2 w-full lg:max-w-md lg:flex-shrink-0">
+				  <div class="drop-shadow-md -mt-2 p-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0">
 					<div class="rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16">
 					  <div class="mx-auto max-w-xs px-8">
 						<p data-aos="fade-up" class="text-base font-semibold text-gray-600">Pay once, own forever<br/>Starting from</p>
@@ -65,7 +65,7 @@ export default function PriceSection() {
 						  <span class="text-5xl font-bold tracking-tight text-gray-900">850k</span>
 						  <span class="text-sm font-semibold leading-6 tracking-wide text-gray-600">IDR</span>
 						</p>
-						<a data-aos="fade-up" target="_blank" href="https://wa.me/6281525977595" class="drop-shadow-md mt-10 block w-auto rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+						<a data-aos="fade-up" target="_blank" href="https://wa.me/6281525977595" class="drop-shadow-md mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
 						Get it Now
 						</a>
 						<p data-aos="fade-up" class="mt-6 text-xs leading-5 text-gray-600">Invoices and receipts available for easy company reimbursement</p>
