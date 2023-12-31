7442a8525ccaa6784810073e032f4407245ee2a5
fitri-hy <155282015+fitri-hy@users.noreply.github.com>
2023-12-31 11:55:12 +0700

    PriceSection.js

--- src/app/component/PriceSection.js
+++ src/app/component/PriceSection.js
@@ -57,7 +57,7 @@ export default function PriceSection() {
 					  </li>
 					</ul>
 				  </div>
-				  <div class="drop-shadow-md -mt-2 p-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0">
+				  <div class="drop-shadow-md -mt-2 p-2 w-full lg:max-w-md lg:flex-shrink-0">
 					<div class="rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16">
 					  <div class="mx-auto max-w-xs px-8">
 						<p data-aos="fade-up" class="text-base font-semibold text-gray-600">Pay once, own forever<br/>Starting from</p>
